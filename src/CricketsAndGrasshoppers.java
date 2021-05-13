dad88273a7072c269b587f31ad4fc78a2936b15e
Ameco <hamecow@gmail.com>
2021-05-12 22:14:37 -0700

    Wrie method for reading prompts

--- src/CricketsAndGrasshoppers.java
+++ src/CricketsAndGrasshoppers.java
@@ -1,4 +1,26 @@
-// Hannah Wenger
-public class CricketsAndGrasshoppers {
+import java.util.Scanner;
 
+// Hannah Wenger
+
+public class CricketsAndGrasshoppers {
+	
+	//Requirement 1 of 6
+	public static int promptNumberReadLine(Scanner s, String prompt, int max) {
+		while(true) {
+		System.out.print(prompt);
+		if(s.hasNextInt()) {
+			int number = s.nextInt();
+			if(number >= 1 && number <= max) {
+				s.nextLine();
+				return number;
+			}
+
+		} else {
+			s.nextLine();
+			System.out.println("That was not a valid number! Please try again.");
+		}
+		
+		}
+		
+	}
 }
